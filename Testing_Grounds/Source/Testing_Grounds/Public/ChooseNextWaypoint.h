e2237cd35e31bff648e9968960a938a6d8c92435
Ryan Marinoff <ryan.marinoff@outlook.com>
2017-11-16 16:28:39 -0500

    Pre 5:218
    Attempt to fix editor issue savepoint

---
+++ Testing_Grounds/Source/Testing_Grounds/Public/ChooseNextWaypoint.h
@@ -0,0 +1,20 @@
+// No Copyright other than what is on Udemy course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTING_GROUNDS_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
